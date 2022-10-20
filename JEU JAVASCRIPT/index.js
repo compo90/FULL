825d3488ffcc1141bff275f83bcd242cebcd094c
compo90 <abccompo90@gmail.com>
2022-10-20 09:42:33 +0000

    ajout de projet

---
+++ JEU JAVASCRIPT/index.js
@@ -0,0 +1,6 @@
+const Holes = document.queryselectorAll(".hole");
+const scores = document.querySelector(".score");
+const Heads = document.querySelector(".head");
+
+let timeout = false;
+let score = 0;
